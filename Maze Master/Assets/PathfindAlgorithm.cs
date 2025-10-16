41a4ed19a84d9cb54c9ce1fb174a066124288fd9
hammone2 <hammone2@my.erau.edu>
2025-10-16 16:00:13 -0700

    Added Maze

---
+++ Maze Master/Assets/PathfindAlgorithm.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PathfindAlgorithm : MonoBehaviour
+{
+    public List<Cell> cells = new List<Cell>();
+    public Cell startPoint;
+    public Cell endPoint;
+
+    private bool mazeSolved = false;
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Cell cell = transform.GetChild(i).GetComponent<Cell>();
+            cells.Add(cell);
+
+            if (cell.gameObject.name == "MazeEnd")
+                endPoint = cell;
+
+            if (cell.gameObject.name == "MazeStart")
+                startPoint = cell;
+        }
+    }
+}
